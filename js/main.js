ae7ba89a16b2dc102300f897f5072926fcb0ed04
Marco Petrini <marcopetrini1994@gmail.com>
2020-04-21 14:45:08 +0200

    modifica stile

--- js/main.js
+++ js/main.js
@@ -7,93 +7,101 @@
  Voto
 */
 
-var inputSearch = $('#search'); // salvo in una variabile il riferimento all'input di ricerca
-var inputSearchVal;
+$(document).ready(function () {
+  
+  var inputSearch = $('#search'); // salvo in una variabile il riferimento all'input di ricerca
+  var inputSearchVal;
 
-// variabili Template Handlebars
-var source = $('#list-template').html();
-var template = Handlebars.compile(source);
+  // variabili Template Handlebars
+  var source = $('#list-template').html();
+  var template = Handlebars.compile(source);
 
-// imposto di default il valore dell'input vuoto
-inputSearch.val("");
+  // imposto di default il valore dell'input vuoto
+  inputSearch.val("");
 
-// associo l'evento click al button di invio ricerca. Cliccando il bottone parte la funzione ajaxPrint
-$('#go').click(ajaxPrint);
+  // associo l'evento click al button di invio ricerca. Cliccando il bottone parte la funzione ajaxPrint
+  $('#go').click(ajaxPrint);
 
-// associo l'evento tastiera all'input di ricerca
-inputSearch.on('keypress', function (e) {  
+  // associo l'evento tastiera all'input di ricerca
+  inputSearch.on('keypress', function (e) {
 
     // se l'utente preme il tasto invio parte la funzione ajaxPrint
-  if (e.keyCode == 13) {
-      
-    ajaxPrint();
-      
+    if (e.keyCode == 13) {
+
+      ajaxPrint();
+
     }
-    
+
   }
-);
+  );
 
-
-
-// funzione che esegue una chiamata ajax e stampa in pagina i risultati
-function ajaxPrint () {
   
-  $('.container').html(''); // rimuovo l'html presente in container, perchè in questo modo i risultati della ricerca precedente vengono eliminati lasciando posto a quelli della nuova ricerca
-
-  inputSearchVal = inputSearch.val(); // salvo il valore dell'input ogni volta che viene premuto il tasto invio
-
-  $.ajax({ // chiamata ajax
-
-    url: "https://api.themoviedb.org/3/search/movie", // url api con end-point
-
-    method: "GET", // metodo
-
-    dataType: 'json',
-
-    data: { // parametri che vongono aggiunti all'url
-      api_key: 'fab78916a45f752a410befc4f3336db2',
-      language: 'it-IT',
-      query: inputSearchVal
-    },
-
-    success: function (result, stato) {
-
-      var arrayResult = result.results; // salvo in una variabile l'array che mi ritorna dalla chiamata ajax
-
-      // eseguo ciclo sull'array per estrapolare le informazioni che mi servono ad ogni item
-      for (let i = 0; i < arrayResult.length; i++) {
-        var arrayItem = arrayResult[i];
-
-        var filmTitle = arrayItem.title; // salvo il titolo di ogni film ritornato
-        console.log(filmTitle);
-        var filmOriginalTitle = arrayItem.original_title; // salvo il titolo originale di ogni film ritornato
-        console.log(filmOriginalTitle);
-        var filmLanguage = arrayItem.original_language; // salvo la lingua di ogni film ritornato
-        console.log(filmLanguage);
-        var filmRank = arrayItem.vote_average; // salvo il voto di ogni film ritornato
-        console.log(filmRank);
-
-        // creo il contenuto nei segnaposto del template per ogni iterazione
-        var context = {
-          title: filmTitle,
-          originalTitle: filmOriginalTitle,
-          language: filmLanguage,
-          rank: filmRank
-        }
-
-        // aggiungo al container i risultati della chiamata ajax attraverso il template
-        $('.container').append(template(context));
-
-      }
-
-    },
-
-    error: function (richiesta, stato, errore) {
-      console.log(errore);
-      console.log(stato);
-      console.log(richiesta);
-
-    }
+  // al click sul logo boolflix l'eventuale scroll torna in alto
+  $('h1').click(function () {
+    $('main').animate({
+      scrollTop: 0
+    }, 600);
   });
 
-}
+
+  // funzione che esegue una chiamata ajax e stampa in pagina i risultati
+  function ajaxPrint() {
+
+    $('.container').html(''); // rimuovo l'html presente in container, perchè in questo modo i risultati della ricerca precedente vengono eliminati lasciando posto a quelli della nuova ricerca
+
+    inputSearchVal = inputSearch.val(); // salvo il valore dell'input ogni volta che viene premuto il tasto invio
+
+    $.ajax({ // chiamata ajax
+
+      url: "https://api.themoviedb.org/3/search/movie", // url api con end-point
+
+      method: "GET", // metodo
+
+      dataType: 'json',
+
+      data: { // parametri che vongono aggiunti all'url
+        api_key: 'fab78916a45f752a410befc4f3336db2',
+        language: 'it-IT',
+        query: inputSearchVal
+      },
+
+      success: function (result, stato) {
+
+        var arrayResult = result.results; // salvo in una variabile l'array che mi ritorna dalla chiamata ajax
+
+        // eseguo ciclo sull'array per estrapolare le informazioni che mi servono ad ogni item
+        for (let i = 0; i < arrayResult.length; i++) {
+
+          var arrayItem = arrayResult[i]; // salvo l'item dell'array
+          var filmTitle = arrayItem.title; // salvo il titolo di ogni film ritornato
+          var filmOriginalTitle = arrayItem.original_title; // salvo il titolo originale di ogni film ritornato
+          var filmLanguage = arrayItem.original_language; // salvo la lingua di ogni film ritornato
+          var filmRank = arrayItem.vote_average; // salvo il voto di ogni film ritornato
+
+          // creo il contenuto nei segnaposto del template per ogni iterazione
+          var context = {
+            title: filmTitle,
+            originalTitle: filmOriginalTitle,
+            language: filmLanguage,
+            rank: filmRank
+          }
+
+          // aggiungo al container i risultati della chiamata ajax attraverso il template
+          $('.container').append(template(context));
+
+        }
+
+      },
+
+      error: function (richiesta, stato, errore) {
+        console.log(errore);
+        console.log(stato);
+        console.log(richiesta);
+
+      }
+    });
+
+  }
+
+});
+
