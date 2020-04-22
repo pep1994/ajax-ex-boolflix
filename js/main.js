b49542d4140b74812fd9bdb7db36bee735e77245
Marco Petrini <marcopetrini1994@gmail.com>
2020-04-22 12:55:42 +0200

    stampa stelle in base al voto del film o serie tv

--- js/main.js
+++ js/main.js
@@ -7,6 +7,14 @@
  Voto
 */
 
+/* Milestone 2:
+Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
+Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene
+Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+
+Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+*/
+
 $(document).ready(function () {
   
   var inputSearch = $('#search'); // salvo in una variabile il riferimento all'input di ricerca
@@ -94,6 +102,27 @@ $(document).ready(function () {
                 var filmLanguage = arrayItem.original_language; // salvo la lingua di ogni film ritornato
                 var filmRank = arrayItem.vote_average; // salvo il voto di ogni film ritornato
 
+                // trasformo il voto in numero intero e lo divido per due 
+                var rankConvert = Math.ceil(filmRank / 2);
+                console.log(rankConvert);
+                console.log(filmRank);
+                var rankStar;
+
+                if (rankConvert == 0) {
+                  rankStar = '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
+                } else if (rankConvert == 1) {
+                   rankStar = '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
+                } else if (rankConvert == 2) {
+                   rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
+                } else if (rankConvert == 3) {
+                   rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
+                } else if (rankConvert == 4) {
+                   rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>';
+                } else {
+                   rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>';
+                }
+                
+
 
                 // se il titolo è uguale al titolo originale non stampo il titolo originale
                 if (filmTitle == filmOriginalTitle) {
@@ -101,7 +130,7 @@ $(document).ready(function () {
                   var context = {
                     title: filmTitle,
                     language: filmLanguage,
-                    rank: filmRank,
+                    rank: rankStar,
                     type: 'Film'
                   }
 
@@ -114,7 +143,7 @@ $(document).ready(function () {
                     strongOriginalTitle: 'Titolo originale: ',
                     originalTitle: filmOriginalTitle,
                     language: filmLanguage,
-                    rank: filmRank,
+                    rank: rankStar,
                     type: 'Film'
                   }
               
@@ -178,13 +207,35 @@ $(document).ready(function () {
             var filmRank = arrayItem.vote_average; // salvo il voto di ogni film ritornato
 
 
+            // trasformo il voto in numero intero e lo divido per due 
+            var rankConvert = Math.ceil(filmRank / 2);
+            console.log(rankConvert);
+            console.log(filmRank);
+
+            var rankStar;
+
+            if (rankConvert == 0) {
+               rankStar = '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
+            } else if (rankConvert == 1) {
+               rankStar = '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
+            } else if (rankConvert == 2) {
+               rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
+            } else if (rankConvert == 3) {
+               rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
+            } else if (rankConvert == 4) {
+               rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>';
+            } else {
+               rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>';
+            }
+
+
             // se il titolo è uguale al titolo originale non stampo il titolo originale
             if (filmTitle == filmOriginalTitle) {
 
               var context = {
                 title: filmTitle,
                 language: filmLanguage,
-                rank: filmRank,
+                rank: rankStar,
                 type: 'SerieTv'
               }
 
@@ -197,7 +248,7 @@ $(document).ready(function () {
                 strongOriginalTitle: 'Titolo originale: ',
                 originalTitle: filmOriginalTitle,
                 language: filmLanguage,
-                rank: filmRank,
+                rank: rankStar,
                 type: 'SerieTv'
               }
 
