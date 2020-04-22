01c73230d406b9ddb079d9c27cb666025f2a1d91
Marco Petrini <marcopetrini1994@gmail.com>
2020-04-22 21:30:23 +0200

    code refactoring

--- js/main.js
+++ js/main.js
@@ -30,10 +30,16 @@ $(document).ready(function () {
   // imposto di default il valore dell'input vuoto
   inputSearch.val("");
 
-  // associo l'evento click al button di invio ricerca. Cliccando il bottone parte la funzione ajaxPrint
+  // associo l'evento click al button di invio ricerca. Cliccando il bottone parte la funzione ajaxPrint, una volta per richiamare i film e un'altra per le serietv
   $('#go').click(function () {  
+
     $('.container').html(''); // rimuovo l'html presente in container, perchè in questo modo i risultati della ricerca precedente vengono eliminati lasciando posto a quelli della nuova ricerca
-    ajaxPrint();
+
+    $('.not-found').remove(); // rimuovo gli eventuali messaggi di errore in modo che se la ricerca non trova nuovamente film o serie tv, il messaggio non si raddoppi
+
+    ajaxPrint('title', 'original_title', 'Film', 'movie', 'La ricerca non ha prodotto risultati di Film'); // funzione che ritorna i film
+
+    ajaxPrint('name', 'original_name', 'SerieTv', 'tv', 'La ricerca non ha prodotto risultati di SerieTv'); // funzione che ritorna le serieTv
 
   });
 
@@ -44,8 +50,12 @@ $(document).ready(function () {
     if (e.keyCode == 13) {
 
       $('.container').html(''); // rimuovo l'html presente in container, perchè in questo modo i risultati della ricerca precedente vengono eliminati lasciando posto a quelli della nuova ricerca
-    // se l'utente preme il tasto invio parte la funzione ajaxPrint
-      ajaxPrint();
+      $('.not-found').remove(); // rimuovo gli eventuali messaggi di errore in modo che se la ricerca non trova nuovamente film o serie tv, il messaggio non si raddoppi
+
+    // se l'utente preme il tasto invio parte la funzione ajaxPrint come per il click sul bottone
+      ajaxPrint('title', 'original_title', 'Film', 'movie', 'La ricerca non ha prodotto risultati di Film');
+
+      ajaxPrint('name', 'original_name', 'SerieTv', 'tv', 'La ricerca non ha prodotto risultati di SerieTv');
 
     }
 
@@ -61,15 +71,21 @@ $(document).ready(function () {
   });
 
 
-  // funzione che esegue una chiamata ajax e stampa in pagina i risultati
-  function ajaxPrint() {
+
+
+
+  // funzione che esegue una chiamata ajax, elabora i risultati e li stampa in pagina 
+  
+  function ajaxPrint (proprietàTitolo, proprietàTitoloOriginale, type, endPoint, notFoundType) {
+    
 
     var inputSearchVal;
+
     inputSearchVal = inputSearch.val(); // salvo il valore dell'input ogni volta che viene premuto il tasto invio
 
     $.ajax({ // chiamata ajax
 
-      url: "https://api.themoviedb.org/3/search/movie", // url api con end-point
+      url: "https://api.themoviedb.org/3/search/" + endPoint, // url api con end-point
 
       method: "GET", // metodo
 
@@ -78,125 +94,7 @@ $(document).ready(function () {
       data: { // parametri che vongono aggiunti all'url
         api_key: 'fab78916a45f752a410befc4f3336db2',
         language: 'it-IT',
-        query: inputSearchVal
-      },
-
-      success: function (result, stato) {
-
-        var arrayResult = result.results; // salvo in una variabile l'array che mi ritorna dalla chiamata ajax
-        console.log(arrayResult);
-        
-          // se l'array è vuoto, cioè non ha risultati, allora stampa il messaggio di errore
-          if (arrayResult.length <= 0) {
-
-            $('.container').append('<h2 class="not-found">La ricerca non ha prodotto risultati di Film</h2>');
-
-            // altrimenti esegui il ciclo e stampa le informazioni in pagina
-          } else {
-
-              // eseguo ciclo sull'array per estrapolare le informazioni che mi servono ad ogni item
-              for (let i = 0; i < arrayResult.length; i++) {
-
-                var arrayItem = arrayResult[i]; // salvo l'item dell'array
-                console.log(arrayItem);
-                
-                var filmTitle = arrayItem.title; // salvo il titolo di ogni film ritornato
-                var filmOriginalTitle = arrayItem.original_title; // salvo il titolo originale di ogni film ritornato
-                var filmLanguage = arrayItem.original_language; // salvo la lingua di ogni film ritornato
-                var filmRank = arrayItem.vote_average; // salvo il voto di ogni film ritornato
-
-                console.log(flags.includes('en.png'));
-                var flagImg = filmLanguage;
-                
-                
-                for (let j = 0; j < flags.length; j++) {
-                  var flagsItem = flags[j];
-
-                  if (flagsItem.includes(filmLanguage)) {
-                    flagImg = '<img src=' + '"' + 'img/' + flagsItem + '.png' + '"' + 'alt=' + '"' + flagsItem + '"' + 'class=' + '"' + 'flag' + '"' + '>';
-                    // $('img[src=' + '"' + 'img/' + flagsItem + '.png' + '"').attr('alt', flagsItem);
-                  } 
-
-                }
-
-                // trasformo il voto in numero intero e lo divido per due 
-                var rankConvert = Math.ceil(filmRank / 2);
-                console.log(rankConvert);
-                console.log(filmRank);
-                var rankStar;
-
-                if (rankConvert == 0) {
-                  rankStar = '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
-                } else if (rankConvert == 1) {
-                   rankStar = '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
-                } else if (rankConvert == 2) {
-                   rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
-                } else if (rankConvert == 3) {
-                   rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
-                } else if (rankConvert == 4) {
-                   rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>';
-                } else {
-                   rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>';
-                }
-                
-
-
-                // se il titolo è uguale al titolo originale non stampo il titolo originale
-                if (filmTitle == filmOriginalTitle) {
-
-                  var context = {
-                    title: filmTitle,
-                    language: flagImg,
-                    rank: rankStar,
-                    type: 'Film'
-                  }
-
-                // altrimenti stampo anche il titolo originale
-                } else {
-
-                  // creo il contenuto nei segnaposto del template per ogni iterazione
-                  context = {
-                    title: filmTitle,
-                    strongOriginalTitle: 'Titolo originale: ',
-                    originalTitle: filmOriginalTitle,
-                    language: flagImg,
-                    rank: rankStar,
-                    type: 'Film'
-                  }
-              
-                }
-
-                // aggiungo al container i risultati della chiamata ajax attraverso il template
-                $('.container').append(template(context));
-            
-              }
-            
-          }
-
-      },
-
-      error: function (richiesta, stato, errore) {
-        
-        console.log(errore);
-        console.log(stato);
-        console.log(richiesta);
- 
-      }
-    });
-
-
-    $.ajax({ // chiamata ajax
-
-      url: "https://api.themoviedb.org/3/search/tv", // url api con end-point
-
-      method: "GET", // metodo
-
-      dataType: 'json',
-
-      data: { // parametri che vongono aggiunti all'url
-        api_key: 'fab78916a45f752a410befc4f3336db2',
-        language: 'it-IT',
-        query: inputSearchVal
+        query: inputSearchVal // il valore inserito dall'utente 
       },
 
       success: function (result, stato) {
@@ -207,7 +105,7 @@ $(document).ready(function () {
         // se l'array è vuoto, cioè non ha risultati, allora stampa il messaggio di errore
         if (arrayResult.length <= 0) {
 
-          $('.container').append('<h2 class="not-found">La ricerca non ha prodotto risultati di SerieTv</h2>');
+          $('.container').after('<h2 class="not-found">' + notFoundType + '</h2>');
 
           // altrimenti esegui il ciclo e stampa le informazioni in pagina
         } else {
@@ -218,10 +116,10 @@ $(document).ready(function () {
             var arrayItem = arrayResult[i]; // salvo l'item dell'array
             console.log(arrayItem);
 
-            var filmTitle = arrayItem.name; // salvo il titolo di ogni film ritornato
-            var filmOriginalTitle = arrayItem.original_name; // salvo il titolo originale di ogni film ritornato
-            var filmLanguage = arrayItem.original_language; // salvo la lingua di ogni film ritornato
-            var filmRank = arrayItem.vote_average; // salvo il voto di ogni film ritornato
+            var filmTitle = arrayItem[proprietàTitolo]; // salvo il titolo di ogni film o serieTv ritornato
+            var filmOriginalTitle = arrayItem[proprietàTitoloOriginale]; // salvo il titolo originale di ogni film o serieTv ritornato
+            var filmLanguage = arrayItem.original_language; // salvo la lingua di ogni film/serieTv ritornato
+            var filmRank = arrayItem.vote_average; // salvo il voto di ogni film/serieTv ritornato
 
 
             // trasformo il voto in numero intero e lo divido per due 
@@ -229,55 +127,80 @@ $(document).ready(function () {
             console.log(rankConvert);
             console.log(filmRank);
 
+            // definisco una variabile che di default ha lo stesso valore della variabile della lingua
             var flagImg = filmLanguage;
 
+            // ciclo for sull'array delle bandiere
             for (let j = 0; j < flags.length; j++) {
+
+              // variabile che salva l'elemento dell'array ad ogni iterazione
               var flagsItem = flags[j];
 
+              // se l'elemento dell'array contiene la lingua allora la variabile flagImg diventerà una stringa che conterrà l'immagine della bandiera di quella lingua, altrineti manterrà il valore impostato di default
               if (flagsItem.includes(filmLanguage)) {
                 flagImg = '<img src=' + '"' + 'img/' + flagsItem + '.png' + '"' + 'alt=' + '"' + flagsItem + '"' + 'class=' + '"' + 'flag' + '"' + '>';
-              } 
+              }
 
             }
 
+            // variabile che contiene le stelle del rank. Cambierà di valore in base al voto del film o serieTv
             var rankStar;
 
+            var $iconStarEmpty = '<i class="far fa-star"></i>';
+            var $iconStarFull = '<i class="fas fa-star"></i>';
+            
+
+            // se è uguale a 0 la variabile conterrà tutte stelle vuote
             if (rankConvert == 0) {
-               rankStar = '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
-            } else if (rankConvert == 1) {
-               rankStar = '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
+
+              rankStar = $iconStarEmpty + $iconStarEmpty + $iconStarEmpty + $iconStarEmpty + $iconStarEmpty;
+              
+              // se è uguale a 1 conterrà una stella piena e 4 vuote
+            } else if (rankConvert == 1) { 
+
+              rankStar = $iconStarFull + $iconStarEmpty + $iconStarEmpty + $iconStarEmpty + $iconStarEmpty;
+              // se è uguale a 2 conterrà due stelle piene e 3 vuote
             } else if (rankConvert == 2) {
-               rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
+
+              rankStar = $iconStarFull + $iconStarFull + $iconStarEmpty + $iconStarEmpty + $iconStarEmpty;
+              // se è uguale a 3 conterrà tre stelle piene e 2 vuote
             } else if (rankConvert == 3) {
-               rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>' + '<i class="far fa-star"></i>';
+
+              rankStar = $iconStarFull + $iconStarFull + $iconStarFull + $iconStarEmpty + $iconStarEmpty;
+              // se è uguale a 4 conterrà quattro stelle piene e 1 vuota
             } else if (rankConvert == 4) {
-               rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="far fa-star"></i>';
+
+              rankStar = $iconStarFull + $iconStarFull + $iconStarFull + $iconStarFull + $iconStarEmpty;
+              // altrimenti conterrà tutte stelle piene
             } else {
-               rankStar = '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>' + '<i class="fas fa-star"></i>';
+
+              rankStar = $iconStarFull + $iconStarFull + $iconStarFull + $iconStarFull + $iconStarFull;
+
             }
 
 
             // se il titolo è uguale al titolo originale non stampo il titolo originale
             if (filmTitle == filmOriginalTitle) {
 
+              // stampo valori nei segnaposto del template
               var context = {
-                title: filmTitle,
-                language: flagImg,
-                rank: rankStar,
-                type: 'SerieTv'
+                title: filmTitle, // titolo del film o serieTv
+                language: flagImg, // bandiera o stringa con lingua del film o serieTv
+                rank: rankStar, // stelle del rank
+                type: type // se film o serieTv
               }
 
               // altrimenti stampo anche il titolo originale
             } else {
 
-              // creo il contenuto nei segnaposto del template per ogni iterazione
+              // stampo valori nei segnaposto del template
               context = {
                 title: filmTitle,
                 strongOriginalTitle: 'Titolo originale: ',
                 originalTitle: filmOriginalTitle,
                 language: flagImg,
                 rank: rankStar,
-                type: 'SerieTv'
+                type: type
               }
 
             }
@@ -291,16 +214,16 @@ $(document).ready(function () {
 
       },
 
+      // nel caso la chiamata ajax non vada a buon fine appare un alert con l'errore
       error: function (richiesta, stato, errore) {
 
-        console.log(errore);
-        console.log(stato);
-        console.log(richiesta);
+        alert(stato);
 
       }
     });
 
   }
+  
   
 });
 
